ce35f059e7607a59211cccaab8094cf69f05ba94
Vigneshwaran Veeran <vigneshwaran.veeran@appdirect.com>
2020-04-11 01:15:19 -0400

    geolocation

--- screens/Home.js
+++ screens/Home.js
@@ -1,29 +1,72 @@
 import React from 'react';
-import { StyleSheet, Dimensions, ScrollView, Alert } from 'react-native';
+import { StyleSheet, Dimensions, ScrollView, Alert,Text, View } from 'react-native';
 import { Block, theme } from 'galio-framework';
 
 import { Card } from '../components';
 import articles from '../constants/articles';
+import ArButton from "../components/Button";
 const { width } = Dimensions.get('screen');
 
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null
+    };
+  }
+
+  componentDidMount() {
+    this.setGeolocationCoordinates();
+
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // if(this.state.latitude !== prevState.latitude || this.state.longitude !== prevState.longitude){
+    //   this.setGeolocationCoordinates();
+    // }
+  }
+
+  setGeolocationCoordinates = () => navigator.geolocation.getCurrentPosition(
+      position => {
+        //const location = JSON.stringify(position);
+        //Alert.alert(location);
+        this.setState({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+      },
+
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+  );
+
   renderArticles = () => {
     return (
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.articles}>
-        <Block flex>
-          <Card item={articles[0]} horizontal  />
-        </Block>
-      </ScrollView>
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.articles}>
+          <Block flex>
+            <Card item={articles[0]} horizontal  />
+          </Block>
+        </ScrollView>
     )
   };
 
   render() {
+
     return (
-      <Block flex center style={styles.home}>
-        {this.renderArticles()}
-      </Block>
+        <>
+          <View>
+            <Text>
+              Latitude : {this.state.latitude}
+            </Text>
+            <Text>
+              Longitude : {this.state.longitude}
+            </Text>
+          </View>
+          <Block flex center style={styles.home}>
+            {this.renderArticles()}
+          </Block>
+        </>
     );
   }
 }
