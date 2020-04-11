366accccf784e678dfa9170a327d6afa5a7fdc48
Vigneshwaran Veeran <vigneshwaran.veeran@appdirect.com>
2020-04-10 23:51:07 -0400

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Dimensions, ScrollView, Alert } from 'react-native';
+import { Block, theme } from 'galio-framework';
+
+import { Card } from '../components';
+import articles from '../constants/articles';
+const { width } = Dimensions.get('screen');
+
+class Home extends React.Component {
+  renderArticles = () => {
+    return (
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.articles}>
+        <Block flex>
+          <Card item={articles[0]} horizontal  />
+        </Block>
+      </ScrollView>
+    )
+  };
+
+  render() {
+    return (
+      <Block flex center style={styles.home}>
+        {this.renderArticles()}
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  home: {
+    width: width,
+  },
+  articles: {
+    width: width - theme.SIZES.BASE * 2,
+    paddingVertical: theme.SIZES.BASE,
+  },
+});
+
+export default Home;
